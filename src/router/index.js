372e2875769bc4fdb081a8e950033668e764129d
c55403225 <55403225@qq.com>
2020-12-23 17:35:30 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import BMap from '../views/BMap.vue'
-import BMap2 from '../views/BMap2.vue'
-import LiquidFill from '../views/LiquidFill.vue'
+
 
 Vue.use(VueRouter)
 
@@ -12,17 +10,7 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/bmap',
-    name: 'BMap',
-    component: BMap
-  },
-  {
-    path: '/bmap2',
-    name: 'BMap2',
-    component: BMap2
-  },
+  }
  
 ]
 
